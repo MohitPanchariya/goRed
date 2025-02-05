b2fc4c2f47e42502eeddee5d30894f004b82a0ea
Mohit Panchariya <iammohitpanchariya@gmail.com>
2025-02-05 23:11:32 +0530

    Add expiry options to SET command
    The SET handler now allows four different kinds of
    expiry options
    1. EX - expiry time in seconds
    2. PX - expiry time in milliseconds
    3. EXAT - expiry time as a unix timestamp(seconds)
    4. PXAT - expiry time as a unix timestamp(milliseconds)
    
    Additionally, it provides two options
    1. NX - Only set the key if it does not already exist
    2. XX - Only set the key if it already exists
    
    As of now, the GET command doesn't respect the expiry
    time and returns the value of the key regardless.

--- handlers.go
+++ handlers.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"strconv"
 	"sync"
 	"time"
 
@@ -42,12 +43,13 @@ func (s *store) get(key string) ([]byte, bool) {
 
 // `set` is used to set the value of a key in a
 // concurrency safe manner
-func (s *store) set(key string, value []byte) {
+func (s *store) set(key string, value []byte, expire time.Time) {
 	s.lock.Lock()
 	defer s.lock.Unlock()
 	s.db[key] = redisValue{
 		valueType: "string",
 		value:     value,
+		expire:    expire,
 	}
 }
 
@@ -98,8 +100,111 @@ func get(args [][]byte, s *store) ([]byte, error) {
 
 // SET command is used to set the value of a key
 func set(args [][]byte, s *store) ([]byte, error) {
-	s.set(string(args[0]), args[1])
-	var response resp.SimpleString
-	response.Data = "OK"
-	return response.Serialise()
+	var nx, xx, ex, px, exat, pxat, get bool
+	setCounter := 0
+	expiryOptionCounter := 0
+	timeArgs := 0
+	key := string(args[0])
+	value := args[1]
+	currentValue, keyExists := s.get(key)
+	for i := 2; i < len(args); i++ {
+		switch string(args[i]) {
+		case "NX":
+			setCounter++
+			nx = true
+		case "XX":
+			setCounter++
+			xx = true
+		case "EX":
+			expiryOptionCounter++
+			timeArgs = i + 1
+			ex = true
+		case "PX":
+			expiryOptionCounter++
+			timeArgs = i + 1
+			px = true
+		case "EXAT":
+			expiryOptionCounter++
+			timeArgs = i + 1
+			exat = true
+		case "PXAT":
+			expiryOptionCounter++
+			timeArgs = i + 1
+			pxat = true
+		}
+	}
+	// error cases
+	if (setCounter > 1) || (expiryOptionCounter > 1) || (timeArgs >= len(args)) {
+		response := resp.SimpleError{
+			Data: "invalid syntax",
+		}
+		serialised, err := response.Serialise()
+		if err != nil {
+			return nil, err
+		}
+		return serialised, nil
+	}
+	// cases where key shouldn't be set
+	if (nx && keyExists) || (xx && !keyExists) {
+		var response resp.RESPDatatype
+		// if the key doesn't exist, nil must be returned in either
+		// of the above cases
+		if get && keyExists {
+			data, _ := s.get(key)
+			response = &resp.SimpleString{
+				Data: string(data),
+			}
+		} else {
+			response = &resp.BulkString{
+				Size: -1,
+			}
+		}
+		serialised, err := response.Serialise()
+		if err != nil {
+			return nil, err
+		}
+		return serialised, err
+	}
+
+	var expiration time.Time
+	var parsedTime int
+	var err error
+	if expiryOptionCounter > 0 {
+		parsedTime, err = strconv.Atoi(string(args[timeArgs]))
+		if err != nil {
+			return nil, err
+		}
+		currentTime := time.Now()
+		if ex {
+			expiration = currentTime.Add(time.Second * time.Duration(parsedTime))
+		} else if px {
+			expiration = currentTime.Add(time.Millisecond * time.Duration(parsedTime))
+		} else if exat {
+			expiration = time.Unix(int64(parsedTime), 0)
+		} else if pxat {
+			expiration = time.UnixMilli(int64(parsedTime))
+		}
+	}
+	s.set(key, value, expiration)
+	var response resp.RESPDatatype
+	if get && keyExists {
+		if keyExists {
+			response = &resp.SimpleString{
+				Data: string(currentValue),
+			}
+		} else {
+			response = &resp.BulkString{
+				Size: -1,
+			}
+		}
+	} else {
+		response = &resp.SimpleString{
+			Data: "OK",
+		}
+	}
+	serialised, err := response.Serialise()
+	if err != nil {
+		return nil, err
+	}
+	return serialised, nil
 }
