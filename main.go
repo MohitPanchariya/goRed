0f51c11299b7814721135424c95c295d09893589
Mohit Panchariya <iammohitpanchariya@gmail.com>
2025-02-08 16:15:44 +0530

    Use the same TCP connection until client closes it
    The same TCP connection is now used for multiple client command
    excutions. This helps significantly improve performance, by preventing
    tearing down and setting up a TCP connections for each request.

--- main.go
+++ main.go
@@ -28,70 +28,75 @@ func dispatch(c net.Conn) {
 
 func dispatchHelper(c net.Conn) error {
 	reader := bufio.NewReader(c)
-	var err error
-	// command stores the command and arguments passed
-	var command [][]byte
-	// arrayToken is made up of ARRAY_IDENTIFIER<size>TERMINATOR
-	arrayToken, err := reader.ReadBytes('\n')
-	if err != nil {
-		return resp.ErrTerminatorNotFound
-	}
-	if string(arrayToken[0]) != resp.ARRAY_IDENTIFIER {
-		return resp.ErrInvalidClientData
-	}
-	arraySize, err := strconv.Atoi(string(arrayToken[1 : len(arrayToken)-resp.TERMINATOR_SIZE]))
-	if err != nil {
-		return resp.ErrLengthExtraction
-	}
-	// read one bulk string at a time
-	for i := 0; i < arraySize; i++ {
-		bulkString, err := reader.ReadBytes('\n')
+	// read from the TCP connection until its closed
+	for {
+		// command stores the command and arguments passed
+		var command [][]byte
+		// arrayToken is made up of ARRAY_IDENTIFIER<size>TERMINATOR
+		arrayToken, err := reader.ReadBytes('\n')
 		if err != nil {
-			return resp.ErrTerminatorNotFound
+			// no data read and EOF implies that the client closed the connection
+			if len(arrayToken) == 0 {
+				return nil
+			} else { // abrupt termination of the connection
+				return resp.ErrTerminatorNotFound
+			}
 		}
-		// extract length
-		length, err := strconv.Atoi(string(bulkString[1 : len(bulkString)-resp.TERMINATOR_SIZE]))
+		if string(arrayToken[0]) != resp.ARRAY_IDENTIFIER {
+			return resp.ErrInvalidClientData
+		}
+		arraySize, err := strconv.Atoi(string(arrayToken[1 : len(arrayToken)-resp.TERMINATOR_SIZE]))
 		if err != nil {
 			return resp.ErrLengthExtraction
 		}
-		bulkStringData := make([]byte, length)
-		copied, err := io.ReadFull(reader, bulkStringData)
+		// read one bulk string at a time
+		for i := 0; i < arraySize; i++ {
+			bulkString, err := reader.ReadBytes('\n')
+			if err != nil {
+				return resp.ErrTerminatorNotFound
+			}
+			// extract length
+			length, err := strconv.Atoi(string(bulkString[1 : len(bulkString)-resp.TERMINATOR_SIZE]))
+			if err != nil {
+				return resp.ErrLengthExtraction
+			}
+			bulkStringData := make([]byte, length)
+			copied, err := io.ReadFull(reader, bulkStringData)
+			if err != nil {
+				return err
+			}
+			if copied != len(bulkStringData) {
+				return resp.ErrBulkStringDataSize
+			}
+			// read the terminator
+			_, err = reader.ReadBytes('\n')
+			if err != nil {
+				return resp.ErrTerminatorNotFound
+			}
+			// add command/arg to the data without the TERMINATOR
+			command = append(command, bulkStringData)
+		}
+		var serialisedData []byte
+		switch string(command[0]) {
+		case "PING":
+			serialisedData, err = ping(command[1:])
+		case "ECHO":
+			serialisedData, err = echo(command[1:])
+		case "GET":
+			serialisedData, err = get(command[1:], keyValueStore)
+		case "SET":
+			serialisedData, err = set(command[1:], keyValueStore)
+		case "EXISTS":
+			serialisedData, err = exists(command[1:], keyValueStore)
+		case "DEL":
+			serialisedData, err = del(command[1:], keyValueStore)
+		}
 		if err != nil {
 			return err
+		} else {
+			c.Write(serialisedData)
 		}
-		if copied != len(bulkStringData) {
-			return resp.ErrBulkStringDataSize
-		}
-		// read the terminator
-		_, err = reader.ReadBytes('\n')
-		if err != nil {
-			return resp.ErrTerminatorNotFound
-		}
-		// add command/arg to the data without the TERMINATOR
-		command = append(command, bulkStringData)
 	}
-	var serialisedData []byte
-	switch string(command[0]) {
-	case "PING":
-		serialisedData, err = ping(command[1:])
-	case "ECHO":
-		serialisedData, err = echo(command[1:])
-	case "GET":
-		serialisedData, err = get(command[1:], keyValueStore)
-	case "SET":
-		serialisedData, err = set(command[1:], keyValueStore)
-	case "EXISTS":
-		serialisedData, err = exists(command[1:], keyValueStore)
-	case "DEL":
-		serialisedData, err = del(command[1:], keyValueStore)
-	}
-	if err != nil {
-		return err
-	} else {
-		c.Write(serialisedData)
-	}
-	c.Close()
-	return nil
 }
 
 func main() {
